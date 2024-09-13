6088dcbdcbf019090a594071db5d1de220d8bc89
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-09-13 13:20:57 +0200

    VIdeo 3

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>contact</title>
+    <title>Home page</title>
 </head>
 <body>
-    <h1> Contact Page</h1>
+    <nav>
+      <a href="/">Home</a> 
+      <a href="/about">About</a> 
+      <a href="/contact">Contact</a> 
+    </nav>
+  {{$slot}}
 </body>
 </html>
