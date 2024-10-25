0097d6f18c9c3f94f4f0be7594936223139d9c09
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-10-25 13:27:28 +0200

    episode 15 af

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +18,12 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'john',
+            'last_name' => 'doe',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(JobSeeder::class);
+
     }
 }
