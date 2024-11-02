f07533d381c1971f03658d8a121e123334ab4a6f
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-11-02 21:32:34 +0100

    Video 19 Af

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function index()
+    {
+        $jobs = Job::with('employer')->latest()->simplePaginate(3);
+        
+        return view('jobs.index', [ 
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function store(Request $request)
+    {
+        // Validate
+        $request->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => 'required'
+        ]);
+
+        // Create job
+        Job::create([
+            'title' => $request->input('title'),
+            'salary' => $request->input('salary'),
+            'employer_id' => 1 // Adjust this as needed
+        ]);
+
+        // Redirect to the jobs index page
+        return redirect('/jobs');
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ['job' => $job]);
+    }
+
+    public function update(Request $request, Job $job)
+    {
+        // Validate
+        $request->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => 'required'
+        ]);
+
+        // Update job
+        $job->update([
+            'title' => $request->input('title'),
+            'salary' => $request->input('salary')
+        ]);
+
+        // Redirect to the job page
+        return redirect('/jobs/' . $job->id);
+    }
+
+    public function destroy(Job $job)
+    {
+        // Delete the job
+        $job->delete();
+
+        return redirect('/jobs');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,95 +1,12 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::Get('/', function () {
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-   $jobs =  job::all();
-
-
-
-    return view('Home');
-});
+Route::resource('jobs', JobController::class);
 
 
-Route::Get('/jobs', function ()  {
-    $jobs = job:: with ('employer')->latest()->simplePaginate(3);
 
-    return view('jobs.index', [ 
-        'jobs' => $jobs
-    ]);
-});
-
-//Create
-Route::Get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-// Show
-Route::Get('/jobs/{id}', function ($id)   {
-    $job = Job::find($id); // This now correctly references the Job model
-
-    return view('jobs.show', ['job' => $job]);
-});
-
-// Store
-  Route::post('/jobs' , function () {
-  request()->validate([
-    'title' => ['required', 'min:3'],
-    'salary' => 'required'
-  ]);
-
-  job::create([
-    'title' => request ('title'),
-    'salary' => request ('salary'),
-    'employer_id' => 1
-  ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::findOrFail($id); // Correctly references the Job model
-
-    return view('jobs.edit', ['job' => $job]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    // Validate
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => 'required'
-    ]);
-
-    // Authorize
-
-    // Update job
-    $job = Job::findOrFail($id); // Correct method name
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    // Redirect to the job page
-    return redirect("/jobs/{$id}");
-});
-
-// Destroy
-Route::delete('/jobs/{id}', function ($id) {
-    // Authorize
-
-    // Delete the job
-    $job = Job::findOrFail($id); // Correct method name
-    $job->delete();
-
-    return redirect('/jobs');
-});
-  
-
-Route::Get('/contact', function () {
-    return view('contact');
-});
