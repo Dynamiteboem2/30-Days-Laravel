003be55d77e7fd7e5c97a71abdf847311404addd
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-11-02 20:53:29 +0100

    video 18 af

--- routes/web.php
+++ routes/web.php
@@ -21,17 +21,19 @@ Route::Get('/jobs', function ()  {
     ]);
 });
 
+//Create
 Route::Get('/jobs/create', function () {
     return view('jobs.create');
 });
 
-
+// Show
 Route::Get('/jobs/{id}', function ($id)   {
     $job = Job::find($id); // This now correctly references the Job model
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
   Route::post('/jobs' , function () {
   request()->validate([
     'title' => ['required', 'min:3'],
@@ -43,6 +45,46 @@ Route::Get('/jobs/{id}', function ($id)   {
     'salary' => request ('salary'),
     'employer_id' => 1
   ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::findOrFail($id); // Correctly references the Job model
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id) {
+    // Validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => 'required'
+    ]);
+
+    // Authorize
+
+    // Update job
+    $job = Job::findOrFail($id); // Correct method name
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary')
+    ]);
+
+    // Redirect to the job page
+    return redirect("/jobs/{$id}");
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id) {
+    // Authorize
+
+    // Delete the job
+    $job = Job::findOrFail($id); // Correct method name
+    $job->delete();
 
     return redirect('/jobs');
 });
