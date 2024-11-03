2da0f48506b613082228df2c567fbca6a0d60d29
Dynamiteboem2 <danielvangrol2006@gmail.com>
2024-11-03 14:18:23 +0100

    Video 22 Af

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +16,21 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        // Validate
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', Password::min(5), 'confirmed'],
+        ]);
+
+        // Create the user
+        $user = User::create($attributes);
+
+        // Log in
+        Auth::login($user);
+
+        // Redirect
+        return redirect('/jobs');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,9 +33,18 @@
             <x-nav-link href="/login" :active="request()->is('login')">Log in</x-nav-link>
             <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
           @endguest
+
+          @auth
+           <form method="post" action="/logout">
+            @csrf
+            <x-form-button>Log out</x-form-button>
+           </form>
+          @endauth
+
             </div>
           </div>
         </div>
+
         <div class="-mr-2 flex md:hidden">
           <!-- Mobile menu button -->
           <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
